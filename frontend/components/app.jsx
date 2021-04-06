a2f0357a0868233aed510f17c3701b37e1ea223d
Mike <michaeldeanmdmph@gmail.com>
2021-04-05 23:46:03 -0700

    add next commit

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import {Route, Switch} from 'react-router-dom';
+import GreetingContainer from "./greeting/greeting_container";
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
 
+const App = () => (
+  <div>
+    <header>
+    <h1>mikEtsy</h1>
+    <GreetingContainer />
+    </header>
+    <Switch>
+      <Route exact path="/login" component={LogInFormContainer} />
+      <Route exact path="/signup" component={SignUpFormContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
--- frontend/mikEtsy.jsx
+++ frontend/mikEtsy.jsx
@@ -1,9 +1,16 @@
-//React
-
-import React from "react";
+import React from "react"
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+
+  // // TESTING START
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // // TESTING END
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>mikEtsy</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
