f4114a8331aa2c8cdfef8f1795247f737baffb82
Mike <michaeldeanmdmph@gmail.com>
2021-04-10 20:48:17 -0700

    add next commit

--- frontend/components/products/product_index_item.jsx
+++ frontend/components/products/product_index_item.jsx
@@ -2,20 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProductIndexItem = ({ product, deleteProduct, currentUserId }) => {
-  // const restrictedDelete = (product) => {
-  //   return currentUserId && currentUserId === product.user_id
-  //     ? deleteProduct(product.id)
-  //     : null;
-  // };
+
   return (
     <div>
       <li>
         <Link to={`/products/${product.id}`}>
         <div className="product-image">IMAGE PLACE HOLDER</div>
           {product.title}</Link>
-        {/* <Link to={`/products/${product.id}/edit`}>Edit</Link> */}
-        {/* <button onClick={()=> deleteProduct(product.id)}>Delete</button> */}
-        {/* <button onClick={() => restrictedDelete(product)}>Delete</button> */}
+
       </li>
     </div>
   );
