48b4d70fb0a43c04fee457c918d784234fadb8d3
Mike <michaeldeanmdmph@gmail.com>
2021-04-13 23:04:01 -0700

    add next commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_031720) do
+ActiveRecord::Schema.define(version: 2021_04_14_055824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,18 @@ ActiveRecord::Schema.define(version: 2021_04_12_031720) do
     t.string "photoUrl"
     t.index ["order_id"], name: "index_products_on_order_id"
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "rating", null: false
+    t.integer "product_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  rating     :integer          not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_reviews_on_product_id  (product_id)
+#  index_reviews_on_user_id     (user_id)
+#
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
