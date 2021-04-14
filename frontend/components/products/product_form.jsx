8babb0ba16fb59b0f1998229a6989dd2ed5c5c5c
Mike <michaeldeanmdmph@gmail.com>
2021-04-13 21:55:13 -0700

    add next commit

--- frontend/components/products/product_form.jsx
+++ frontend/components/products/product_form.jsx
@@ -64,30 +64,41 @@ class ProductForm extends React.Component {
     }
     return (
       <div className="product-form-container">
-        <div className="form-title">{(this.props.formType === 'Add a new listing') ? this.props.formType : this.state.title}</div>
+        <div className="form-title">
+          {this.props.formType === "Add a new listing"
+            ? this.props.formType
+            : this.state.title}
+        </div>
 
         <form onSubmit={this.handleSubmit}>
-
-          <div className="photo-input">            
-              <div className="photo-label">Photos</div>
-              <div className="photo-but-container">
+          <div className="photo-input">
+            <div className="photo-label">Photos</div>
+            <div className="ld-pairs">
+            <div className="photo-instructions">
+              Add a photo so buyers can see your sweet basket of goodness!
+            </div>
+            <div className="photo-but-container">
               <input
                 className="photo-select-but"
                 type="file"
                 onChange={this.imagePreview}
-                />            
-                <img className="preview-image" src={this.state.photoUrl} alt="" />
-              </div>
+                />
+              <img className="preview-image" src={this.state.photoUrl} alt="" />
+            </div>
+                </div>
           </div>
 
           <div className="listing-details">
-          <div className="listing-details-text">Listing details</div> 
+            <div className="listing-details-text">Listing details</div>
             <div className="ld-pairs">
-            <div className="listing-details-title-text-area">
-              <div className="listing-details-title-text">Title</div> 
-            <br/>
-            Include keywords that buyers would use to search for your item.
-            </div> 
+              <div className="listing-details-title-text-area">
+                <div className="listing-details-title-text">Title</div>
+
+                <div className="cat-explanation">
+                  Include keywords that buyers would use to search for your
+                  item.
+                </div>
+              </div>
               <input
                 type="text"
                 onChange={this.update("title")}
@@ -95,7 +106,15 @@ class ProductForm extends React.Component {
               />
             </div>
             <div className="ld-pairs">
-                  <div className="description-text">Description</div>  
+              <div className="description-text-area">
+                <div className="description-text">Description</div>
+                <div className="cat-explanation">
+                  Start with a brief overview that describes your item’s finest
+                  features. Shoppers will only see the first few lines of your
+                  description at first, so make it count!
+                </div>
+              </div>
+
               <textarea
                 onChange={this.update("description")}
                 value={this.state.description}
@@ -103,19 +122,31 @@ class ProductForm extends React.Component {
             </div>
           </div>
           <div className="inventory-pricing">
-          <div className="inv-price-text">Inventory and pricing</div>   
-            <div>
-              Price
-              <input
+            <div className="inv-price-text">Inventory and pricing</div>
+            <div className="ld-pairs">
+             <div>
+             <div className="price-text">Price</div> 
+              <div>
+                  Remember to factor in the costs of materials, labor, and other
+                  business expenses. If you offer free shipping, make sure to
+                  include the cost of shipping so it doesn't eat into your
+                  profits.
+                </div>
+             </div>
+             <div className="price-input-field">
+                <input
                 type="number"
                 min="0"
                 onChange={this.update("price")}
                 value={this.state.price}
               />
+             </div>
+             
             </div>
           </div>
 
-          <input type="submit" />
+          {/* <input className="publish-button" type="submit"/> */}
+          <button className="publish-button" type="submit">Publish</button>
         </form>
       </div>
     );
