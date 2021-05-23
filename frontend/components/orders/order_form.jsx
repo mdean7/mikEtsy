633a04042e9776b397fe2bfbcb2af521f81fe48f
Mike <michaeldeanmdmph@gmail.com>
2021-05-22 20:20:47 -0700

    add next commit

--- frontend/components/orders/order_form.jsx
+++ frontend/components/orders/order_form.jsx
@@ -14,6 +14,11 @@ class OrderForm extends React.Component {
     this.props.deleteOrder(id);
   }
 
+  handleUpdate(value, order){
+    order.total = value
+    this.props.updateOrder(order, order.id)
+  }
+
   render() {
     if (!this.props.products) {
       return null;
@@ -37,7 +42,7 @@ class OrderForm extends React.Component {
       }
     }
     return (
-      <div>
+      <div className='cart-container'>
         {orderProducts.length > 0 && this.props.currentUserId ? (
           <div>
             {orderProducts.length}{" "}
@@ -48,43 +53,60 @@ class OrderForm extends React.Component {
                   key={Math.floor((product[1].id + 3) * 100000 * Math.random())}
                 >
                   <div className="cart-item-container">
+                    <div className="card-and-info">
+
                     <ProductIndexItem
                       key={Math.floor(
                         (product[1].description + 1) * 100000 * Math.random()
-                      )}
-                      product={product[0]}
-                      currentUserId={this.props.currentUserId}
-                    />
+                        )}
+                        product={product[0]}
+                        currentUserId={this.props.currentUserId}
+                        />
                     <div className="cart-info">
                       <div>{product[0].title} </div>
                       <div>{product[0].description} </div>
-                      <button onClick={() => this.handleDelete(product[1].id)}>
-                        Remove from cart
-                      </button>
                     </div>
-                    <div className="product-total">
-                      <select >
-                        <option value={product[1].total}>{product[1].total}</option>
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='4'>4</option>
-                        <option value='5'>5</option>
-                      </select>
+                        </div>
                       {/* {product[1].total} */}
-                    </div>
+                    <div className="select-and-remove">
+                      <div className="product-total">
+                        <select className="total-select" onChange={(e)=>this.handleUpdate(e.target.value, product[1])} >
+                          <option value={product[1].total}>{product[1].total}</option>
+                          <option value='1'>1</option>
+                          <option value='2'>2</option>
+                          <option value='3'>3</option>
+                          <option value='4'>4</option>
+                          <option value='5'>5</option>
+                        </select>
+                      </div>
+                        <button onClick={() => this.handleDelete(product[1].id)}>
+                          Remove
+                        </button>
+                        {"$" + product[0].price * product[1].total}
+                      </div>
                   </div>
+                  {/* above this point you have access to mapped products and order */}
                 </div>
               );
             })}
+
+
+
+
+
           </div>
         ) : this.props.currentUserId ? (
           <div className="empty-cart">Your Cart is Empty</div>
-        ) : (
-          <div className="empty-cart">
+          ) : (
+            <div className="empty-cart">
             Please sign in to add items to your cart
           </div>
         )}
+        <div className='checkout-box'>How you'll pay
+          <radio></radio>
+            
+
+        </div>
       </div>
     );
   }
