d5fadf97585154f7fa1dffff5c8f42bba3289eb3
Mike <michaeldeanmdmph@gmail.com>
2021-06-30 12:47:45 -0700

    add next commit

--- frontend/actions/order_actions.js
+++ frontend/actions/order_actions.js
@@ -1,5 +1,4 @@
-import * as OrderAPIUtil from '../util/order_api_util';
-
+import * as OrderAPIUtil from "../util/order_api_util";
 
 export const RECEIVE_ORDERS = "RECEIVE_ORDERS";
 export const RECEIVE_ORDER = "RECEIVE_ORDER";
@@ -7,38 +6,36 @@ export const REMOVE_ORDER = "REMOVE_ORDER";
 
 export const receiveOrders = (orders) => ({
   type: RECEIVE_ORDERS,
-  orders
+  orders,
 });
 
 export const receiveOrder = (order) => ({
   type: RECEIVE_ORDER,
-  order
+  order,
 });
 
 export const removeOrder = (orderId) => ({
   type: REMOVE_ORDER,
-  orderId
+  orderId,
 });
 
-export const requestOrders = () => (dispatch)=> (
-  OrderAPIUtil.fetchOrders()
-  .then((orders) => dispatch(receiveOrders(orders)))
-);
+export const requestOrders = () => (dispatch) =>
+  OrderAPIUtil.fetchOrders().then((orders) => dispatch(receiveOrders(orders)));
 
-export const requestOrder = (orderId) => (dispatch) =>(
-  OrderAPIUtil.fetchOrder(orderId)
-  .then((order) =>dispatch(receiveOrder(order)))
-);
+export const requestOrder = (orderId) => (dispatch) =>
+  OrderAPIUtil.fetchOrder(orderId).then((order) =>
+    dispatch(receiveOrder(order))
+  );
 
-export const createOrder = (order) => (dispatch)=> (
-  OrderAPIUtil.createOrder(order)
-  .then((order) => dispatch(receiveOrder(order)))
-);
+export const createOrder = (order) => (dispatch) =>
+  OrderAPIUtil.createOrder(order).then((order) =>
+    dispatch(receiveOrder(order))
+  );
 
-export const updateOrder = (order, id) => (dispatch)=> (
-  OrderAPIUtil.updateOrder(order, id)
-  .then((order) => dispatch(receiveOrder(order)))
-);
+export const updateOrder = (order, id) => (dispatch) =>
+  OrderAPIUtil.updateOrder(order, id).then((order) =>
+    dispatch(receiveOrder(order))
+  );
 
 export const deleteOrder = (id) => (dispatch) => {
   return OrderAPIUtil.deleteOrder(id).then((order) =>
