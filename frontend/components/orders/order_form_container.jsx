ef513b241d87ee7fc5cb1aec6bac8fde2eca30c1
mike <michaeldeanmdmph@gmail.com>
2021-07-06 20:17:29 -1000

    add next commit

--- frontend/components/orders/order_form_container.jsx
+++ frontend/components/orders/order_form_container.jsx
@@ -1,15 +1,16 @@
 
 import { connect } from 'react-redux';
-import { requestOrders, createOrder, deleteOrder, updateOrder, removeOrder} from '../../actions/order_actions';
+import { requestOrders, createOrder, deleteOrder, updateOrder, removeOrder } from '../../actions/order_actions';
 import OrderForm from './order_form';
 import { openModal } from "../../actions/modal_actions"
 
-const mapStateToProps = state => { 
-    return ({
+const mapStateToProps = state => {
+  return ({
     orderItems: Object.values(state.orders),
     currentUserId: state.session.id,
     products: Object.values(state.products),
-})}
+  })
+}
 
 const mapDispatchToProps = dispatch => ({
   requestOrders: () => dispatch(requestOrders()),
